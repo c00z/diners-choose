a70c236646181318ae6a1961c52341f8d414c498
c00z <zcusimano@gmail.com>
2017-01-12 15:44:38 -0800

    added background images

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 console.log('Sanity Check');
 
 //Materialze!
-var app = angular.module('materializeApp', ['ui.materialize'])
-    .controller('BodyController', ["$scope", function ($scope) {
-        $scope.select = {
-            value: "Option1",
-            choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
-        };
-    }]);
+// var app = angular.module('materializeApp', ['ui.materialize'])
+//     .controller('BodyController', ["$scope", function ($scope) {
+//         $scope.select = {
+//             value: "Option1",
+//             choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+//         };
+//     }]);
 
 
-angular.module('dinerApp', ['ngRoute'])
+angular.module('dinerApp', ['ngRoute', 'ui.materialize'])
        .config(config);
 
 
