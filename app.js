a7012f658f168da6a9510c459349afc0959c3c1a
c00z <zcusimano@gmail.com>
2017-01-12 14:42:07 -0800

    ngview working properly

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 console.log('Sanity Check');
+
+//Materialze!
+var app = angular.module('materializeApp', ['ui.materialize'])
+    .controller('BodyController', ["$scope", function ($scope) {
+        $scope.select = {
+            value: "Option1",
+            choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+        };
+    }]);
+
+
 angular.module('dinerApp', ['ngRoute'])
        .config(config);
 
