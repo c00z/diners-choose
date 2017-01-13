d846245e2081a36246e4022bfeb4e658afcd5eaa
c00z <zcusimano@gmail.com>
2017-01-12 20:18:15 -0800

    hardcoded text appearing through handle bars

--- controllers/RestIndexController.js
+++ controllers/RestIndexController.js
@@ -2,12 +2,19 @@ angular.module('dinerApp')
   .controller('RestIndexController', RestIndexController);
 
 
-// controller function and dependency injection
-// $routeParams and $location are required for routing stuff
-//   - but you might need to add a dependency
 RestIndexController.$inject=['$http','$routeParams', '$location'];
 function RestIndexController($http, $routeParams, $location) {
   var vm = this;
-
+  vm.newRestaurant = {};
+  vm.newRestaurant = {
+    name: 'HARD CODED in App.js',
+    description: 'OUT',
+    location: '370 14th St, San Francisco, CA 94103',
+    url: 'https://www.yelp.com/biz/shizen-vegan-sushi-bar-and-izakaya-san-francisco',
+    coordinates: {
+      lat: 37.768576,
+      lng: -122.421622
+      }
+    };
 
 }
