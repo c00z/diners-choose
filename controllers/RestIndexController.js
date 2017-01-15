409c829b18c1c5cbd7a05ba65381bd0e89738ff8
c00z <zcusimano@gmail.com>
2017-01-15 09:31:20 -0800

    images to list items

--- controllers/RestIndexController.js
+++ controllers/RestIndexController.js
@@ -11,6 +11,7 @@ function RestIndexController($http, $routeParams, $location) {
     description: 'Vegan Sushi Bar and Izakaya',
     address: '370 14th St, San Francisco, CA 94103',
     url: 'https://www.yelp.com/biz/shizen-vegan-sushi-bar-and-izakaya-san-francisco',
+    image: 'http://www.grillingcompanion.com/wp-content/uploads/2008/11/pizzacooked640.jpg',
     coordinates: {
       lat: 37.768576,
       lng: -122.421622
@@ -20,6 +21,7 @@ function RestIndexController($http, $routeParams, $location) {
     description: 'Chinese eatery serving vegetarian, vegan & kosher dishes.',
     address: '2026 Irving St, San Francisco, CA 94122',
     url: 'http://www.shangrilavgrest.com/',
+    image: 'http://media.npr.org/assets/img/2010/08/22/foodtruck2_wide-6f9e9521d675047a1eacf347f3d3d46fa6fc71b5-s900-c85.jpg',
     coordinates: {
       lat: 37.763665,
       lng: -122.479805
@@ -29,6 +31,7 @@ function RestIndexController($http, $routeParams, $location) {
     description: 'Fast-food chain offering Mexican fare, including design-your-own burritos, tacos & bowls.',
     address: '232 O\'Farrell St, San Francisco, CA 94102',
     url: 'https://chipotle.com/allergens',
+    image: 'https://www.gha.com/var/ezwebin_site/storage/images/files/gha-images/brands/rydges-hotels/rydges-camperdown-sydney/greek-gourmet-food-safari-of-marrickville/rydges_camperdown_greek-gourmet-safari/5034889-1-eng-GB/Rydges_Camperdown_Greek-Gourmet-Safari_le_page.jpg',
     coordinates: {
       lat: 37.786536,
       lng: -122.408587
@@ -38,6 +41,7 @@ function RestIndexController($http, $routeParams, $location) {
     description: 'Vegan Mexican Cuisine',
     address: '2211 Mission St, San Francisco, CA 94110',
     url: 'http://gracias-madre.com/',
+    image:'https://pantograph0.goldbely.com/uploads/merchant/main_image/309/lindas-gourmet-latkes.fc2feb2122f135440188dd1fca2f740e.jpg',
     coordinates: {
       lat: 37.761800,
       lng: -122.419069
@@ -47,6 +51,7 @@ function RestIndexController($http, $routeParams, $location) {
     description: 'Vegan Sushi Restaurant',
     address: '762 Valencia St, San Francisco, CA 94110',
     url: 'http://www.thechaya.com/',
+    image: 'http://www.gourmetfoodsltd.com/~gourmet/dynimage/galleria/450/image.png',
     coordinates: {
       lat: 37.761006,
       lng: -122.421652
